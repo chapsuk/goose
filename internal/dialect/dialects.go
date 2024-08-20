c6251288bf9b6f8212144ee2a26c6d57a65e9536
Maxim Krasilnikov <m.krasilnikov@space307.com>
2024-08-20 20:03:37 +0300

    Add clickhouse-replicated dialect

--- internal/dialect/dialects.go
+++ internal/dialect/dialects.go
@@ -4,14 +4,15 @@ package dialect
 type Dialect string
 
 const (
-	Postgres   Dialect = "postgres"
-	Mysql      Dialect = "mysql"
-	Sqlite3    Dialect = "sqlite3"
-	Sqlserver  Dialect = "sqlserver"
-	Redshift   Dialect = "redshift"
-	Tidb       Dialect = "tidb"
-	Clickhouse Dialect = "clickhouse"
-	Vertica    Dialect = "vertica"
-	Ydb        Dialect = "ydb"
-	Turso      Dialect = "turso"
+	Postgres             Dialect = "postgres"
+	Mysql                Dialect = "mysql"
+	Sqlite3              Dialect = "sqlite3"
+	Sqlserver            Dialect = "sqlserver"
+	Redshift             Dialect = "redshift"
+	Tidb                 Dialect = "tidb"
+	Clickhouse           Dialect = "clickhouse"
+	ClickhouseReplicated Dialect = "clickhouse-replicated"
+	Vertica              Dialect = "vertica"
+	Ydb                  Dialect = "ydb"
+	Turso                Dialect = "turso"
 )
